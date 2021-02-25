4c0ba8fb49c9da25e2d462b9e3d76efd84f314d9
FawadAhmedRaza <68689723+FawadAhmedRaza@users.noreply.github.com>
2021-02-25 16:49:04 +0500

    .

--- src/components/history.js
+++ src/components/history.js
@@ -2,18 +2,19 @@ import React,{useContext} from "react";
 import {GlobalContext} from "../ContextApi/GlobalContext"
 export const History=()=>{
 const {transactions}=useContext(GlobalContext);
+const {deleteTransaction}=useContext(GlobalContext)
 return(
    <div className="History">
       {
          transactions.map(transaction=>
          (
-         <div class="d-flex">
-         <button>X</button>
+         
+        <div className="History">
          <div className={transaction.amount>0 ? "HistoryDetails-Income":"HistoryDetails-Expense"}>
-         <span>{transaction.description}</span>
+         <button onClick={()=>deleteTransaction(transaction.id)} className="btn-delete">X</button><span>{transaction.description}</span>
          <span>{transaction.amount}</span>
-         </div>
-         </div>
+         </div> </div>
+       
          ))
       }     
    </div>
